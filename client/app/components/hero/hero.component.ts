dff526b23912fbfebe04b5e676608f8ad25c1235
pradeepb53 <pradeepb@shaw.ca>
2017-05-12 16:39:42 -0700

    Added hero.component.ts

---
+++ client/app/components/hero/hero.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import {Hero} from '../../shared/hero';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hero',
+  templateUrl:'hero.component.html',
+})
+
+export class HeroComponent {
+   powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+}
